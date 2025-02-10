cff7d2ee726f46d5ea4b241fcf529e83c78adaad
ALAN HENRIQUE SILVA DE ABREU <alan.hsabreu@senacsp.edu.br>
2025-02-10 17:17:39 -0300

    primeiro commit

---
+++ laravel/resources/views/paginas/cadastrar.blade.php
@@ -0,0 +1,17 @@
+<x-layout titulo="Cadastrar">
+    <form action= "" method="GET">
+        @csrf <!--Abrir a porta dos servidores-->
+        <label name="DataCompromisso"> Data Compromisso </label>
+        <input type="date" name="dataEvento"></br></br>
+
+        <label name="descricao">Descrição</label>
+        <textArea type="text" name="descricaoTexto"></textArea>
+        </br></br>
+        <button type="submit">Salvar</button>
+    </form>
+
+    <a href="/"><button>Voltar</button></a>
+
+
+
+</x-layout>
---
+++ laravel/resources/views/paginas/index.blade.php
@@ -0,0 +1,5 @@
+<x-layout titulo="Página Inicial">
+    <p>Bem vindo<br><br></p>
+    <a href="/cadastrar"><button>Cadastrar</button></a></br>
+    <a href="/consultar"><button>Consultar</button></a><br>
+</x-layout>
